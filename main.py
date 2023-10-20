c3709eba1ec1b416bf733b5b1efc6d224cf8b0c5
Pietro Di Leo <pietrodileo_00@yahoo.it>
2023-10-20 15:04:47 +0200

    Build collection

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from src.exploreJSONcontent import explore_and_save_keys
+from src.buildCollection import Builder
 import json
 
 # Define the input JSON file path and the output file path for structured keys
-json_file_path = "Postman Hospital Base Model New.postman_collection.json"
-output_file_path = "structured_keys.txt"
+#json_file_path = "Postman Hospital Base Model New.postman_collection.json"
+#output_file_path = "structured_keys.txt"
 # Call the explore_and_save_keys function to explore JSON keys and save them to the output file
-explore_and_save_keys(json_file_path, output_file_path)
+#explore_and_save_keys(json_file_path, output_file_path)
 
+base_collection_path = "./models/collection/collectionContent.json"
+ouput_collection_path = "./output/collection.json"
+builder = Builder(base_collection_path)
+builder.load_collection_data()
+builder.build_collection(name="Postman Hospital", descriptionPath= "./models/collection/description.txt")
+builder.load_variables()
+builder.save_collection(ouput_collection_path)
---
+++ src/buildCollection.py
@@ -0,0 +1,50 @@
+import json
+import uuid
+
+class Builder:
+    def __init__(self, base_collection_path):
+        self.base_collection_path = base_collection_path
+        self.collection_content = None
+        self.collection_data = None
+
+    def load_collection_data(self):
+        try:
+            with open(self.base_collection_path, "r", encoding="utf-8") as json_file:
+                self.collection_content = json.load(json_file)
+                self.collection_data = self.collection_content["collection"]
+        except FileNotFoundError:
+            print("Collection file not found.")
+        except json.JSONDecodeError:
+            print("Error decoding JSON in the collection file.")
+
+    def build_collection(self, name, descriptionPath):
+        if self.collection_data:
+            try:
+                # Open the description file
+                with open(descriptionPath, "r", encoding="utf-8") as description_file:
+                    description_content = description_file.read()
+                # Insert the content of description.txt into the "description" field
+                self.collection_data["info"]["description"] = description_content
+                # Generate a collection UUID
+                self.collection_data["info"]["_postman_id"] = str(uuid.uuid4())
+                self.collection_data["info"]["name"] = name
+            except FileNotFoundError:
+                print(descriptionPath + " file not found.")
+                
+        else:
+            print("No collection data loaded. Call load_collection_data first.")
+
+    def load_variables(self):
+        if self.collection_data:
+            self.collection_data["variable"] = self.collection_content["variables"] 
+
+    def save_collection(self, output_file_path):
+        if self.collection_data:
+            try:
+                with open(output_file_path, "w", encoding="utf-8") as json_file:
+                    json.dump(self.collection_data, json_file, indent=4)
+                print(f"Collection data saved to {output_file_path}")
+            except Exception as e:
+                print(f"Error saving collection data: {str(e)}")
+        else:
+            print("No collection data to save. Call load_collection and build_collection first.")
